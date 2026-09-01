e8f20c217174934042ad0c53a7710e51c7ced307
andresbucca <136543655+andresbucca@users.noreply.github.com>
2026-09-01 08:54:03 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='mis-gastos-v1-1';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
